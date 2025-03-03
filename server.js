4b703c92b6c46ad1413cc58e1bea154af8e1ac8a
Maneesh <maneeshbugaliya@gmail.com>
2025-03-03 13:57:15 +0530

    moidfications

--- server.js
+++ server.js
@@ -73,5 +73,5 @@ app.post('/api/send-emails', async (req, res) => {
     
 });
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
